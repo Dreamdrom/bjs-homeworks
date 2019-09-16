a45939a06b60c0403d31874e4c40628068d96b1b
Dreamdrom <drs140@yandex.ru>
2019-09-17 00:39:23 +0300

    Задание 1

---
+++ variables/main.js
@@ -0,0 +1,9 @@
+let algebra;
+let geography;
+let physics;
+let averageValue;
+algebra = 5;
+geography = 3; 
+physics = 4;
+averageValue = (algebra + geography + physics)/3;
+console.log("Средняя оценка " + averageValue);
