cfc127f02a3103110e40ebb5a9cbb4dcdee24b5a
Dreamdrom <drs140@yandex.ru>
2019-09-17 01:26:48 +0300

    Выполнены также задания 2 и 3. Задание 2 выводится неверно

--- variables/main.js
+++ variables/main.js
@@ -7,3 +7,14 @@ geography = 3;
 physics = 4;
 averageValue = (algebra + geography + physics)/3;
 console.log("Средняя оценка " + averageValue);
+
+let name = 'Roman';
+let message = 'Привет, мир! Меня зовут ${name}';
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+let result;
+result = (x * y + 5 * z + x - 1)
+console.log(result);
